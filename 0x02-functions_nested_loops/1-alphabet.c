83ab3e9d2fa0fef65c059207549cbc1dac1a1bb4
queenecrown <queenecrown37@gmail.com>
2022-07-09 15:49:49 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,38 @@
 #include "main.h"
 
+
+
 /**
+ *
  *print_alphabet - Begin entry
+ *
  *Return: 0
+ *
  */
 
+
+
 void print_alphabet(void)
-{
-char c = 'a';
 
-while (c <= 'z')
 {
-_putchar(c);
-c++;
 
-}
-_putchar('\n');
+	char c = 'a';
+
+
+
+	while (c <= 'z')
+
+	{
+
+		_putchar(c);
+
+												c++;
+
+
+											}
+
+	_putchar('\n');
+
+
 
 }
